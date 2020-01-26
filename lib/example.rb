03e4da6817ecd288232d408681ae75fc005cd566
Calvin McDowell <calvinmac40@hotmail.com>
2020-01-26 00:46:48 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,5 +1,15 @@
 # Use this space to code along with the readme
 
+def say_it_ten_times
+  phrase = "Hello World!"
+  10.times{|i| puts phrase}
+end
+
+def say_ten_times 
+  phrase = "What's Up?"
+  10.times{|i| puts phrase}
+end
+
 phrase = "Hello World!"
 puts phrase
 puts phrase
